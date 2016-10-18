a53c069a6a750cdfa563057b3ebfc26baa5db506
Jirayu Yingthawornsuk <me@jirayu.in.th>
2016-10-18 17:05:40 +0700

    initial plugin info

---
+++ youtube-to-wordpress.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: YouTube to WordPress
+ * Plugin URI: https://github.com/IronGhost63/youtube-to-wordpress
+ * Description: Automatic create WordPress Post from YouTube Playlist
+ * Version: 1.0.0
+ * Author: Jirayu Yingthawornsuk
+ * Author URI: http://jirayu.in.th
+ * License: GPL2
+ */
+?>
